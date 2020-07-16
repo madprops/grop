a07ca0dad5cc00da678eb48f03b23b02c7f48992
Auric Vente <easywest@protonmail.com>
2020-07-15 22:12:40 -0600

    Hmm

--- launchers/saveselector.sh
+++ launchers/saveselector.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+is_num() { [ "$1" ] && [ -z "${1//[0-9]}" ] ;}
+
 output=$(echo -e "Save Group 1\nSave Group 2\nSave Group 3\nSave Group 4\n" | rofi -dmenu -width 8 -lines 4)
 num=$(echo "$output" | sed "s/Save Group //")
-cmd="node /home/yo/code/grop/grop.js save g${num}"
-eval $cmd
+
+if is_num "$num"; then
+  cmd="node /home/yo/code/grop/grop.js save g${num}"
+  eval $cmd
+fi
