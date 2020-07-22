3cb8c471f60c24d2eba945f4960012a90165f722
Auric Vente <easywest@protonmail.com>
2020-07-21 20:09:21 -0600

    Restore in menu

--- launchers/saveselector.sh
+++ launchers/saveselector.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 is_num() { [ "$1" ] && [ -z "${1//[0-9]}" ] ;}
 
-output=$(echo -e "Save Group 1\nSave Group 2\nSave Group 3\nSave Group 4\n" | rofi -dmenu -width 8 -lines 4)
-num=$(echo "$output" | sed "s/Save Group //")
+a="Save Group 1\nSave Group 2\nSave Group 3\nSave Group 4\n"
+b="Restore Group 1\nRestore Group 2\nRestore Group 3\nRestore Group 4\n"
+c="${a}${b}"
+
+ans=$(echo -e "$c" | rofi -dmenu -width 10 -lines 8)
+
+if [[ $ans == *"Save"* ]]; then
+  num=$(echo "$ans" | sed "s/Save Group //")
+  cmd="node /home/yo/code/grop/grop.js save g${num}"
+else
+  num=$(echo "$ans" | sed "s/Restore Group //")
+  cmd="node /home/yo/code/grop/grop.js restore g${num}"
+fi
 
 if is_num "$num"; then
-  cmd="node /home/yo/code/grop/grop.js save g${num}"
   eval $cmd
 fi
