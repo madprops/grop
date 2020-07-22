6a1b23b5974f0cf651388809da5ef8fb0b93b764
Auric Vente <easywest@protonmail.com>
2020-07-21 21:02:38 -0600

    Launcher mods

--- launchers/saveselector.sh
+++ launchers/saveselector.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 is_num() { [ "$1" ] && [ -z "${1//[0-9]}" ] ;}
 
-a="Save Group 1\nSave Group 2\nSave Group 3\nSave Group 4\n"
-b="Restore Group 1\nRestore Group 2\nRestore Group 3\nRestore Group 4\n"
-c="${a}${b}"
+open="Open Group 1\nOpen Group 2\nOpen Group 3\nOpen Group 4\n"
+save="\nSave Group 1\nSave Group 2\nSave Group 3\nSave Group 4\n"
+c="${open}${save}"
 
-ans=$(echo -e "$c" | rofi -dmenu -width 10 -lines 8)
+ans=$(echo -e "$c" | rofi -dmenu -width 10 -lines 9)
 
 if [[ $ans == *"Save"* ]]; then
   num=$(echo "$ans" | sed "s/Save Group //")
   cmd="node /home/yo/code/grop/grop.js save g${num}"
 else
-  num=$(echo "$ans" | sed "s/Restore Group //")
+  num=$(echo "$ans" | sed "s/Open Group //")
   cmd="node /home/yo/code/grop/grop.js restore g${num}"
 fi
 
